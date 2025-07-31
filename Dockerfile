8a0376cd1218cba30d0e0fafe5c15124b8c166ec
lits-santonil <talitssantonil@gmail.com>
2025-07-31 13:52:28 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use Java base image
+FROM openjdk:17-jdk-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy the built jar file into the container
+COPY target/demodevops-0.0.1-SNAPSHOT.jar app.jar
+
+# Run the jar file
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/demodevops/demodevops/controller/DevopsController.java
@@ -0,0 +1,14 @@
+package com.demodevops.demodevops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevopsController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from Docker + EC2X! ";
+    }
+
+}
